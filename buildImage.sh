136eed08be0c6a15a16a2893fb8aaf5267c1a305
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2017-08-28 10:59:20 -0700

    add --network to build

--- buildImage.sh
+++ buildImage.sh
@@ -45,6 +45,14 @@ the argument defaults in the Dockerfile will be used.
 EOF
 fi
 
+if [ ! -z "$NETWORK" ]; then
+  echo "NETWORK=$NETWORK"
+  ARGS+="--network $NETWORK "
+else
+  echo "ERROR: Required NETWORK value missing from $CONFIG_FILE"
+  exit 1
+fi
+
 if [ ! -f imageFiles/tmp_tomcat/tomcat.jks ]; then
   echo "imageFiles/tmp_tomcat/tomcat.jks is missing.  Run ./generateTLSCert.sh"
   exit 1
